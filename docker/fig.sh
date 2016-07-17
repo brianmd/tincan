94821913d7d05eee2bcfeeab86c7367bb7b7ff0d
Brian Murphy-Dye <brian@murphydye.com>
2016-07-17 14:24:04 -0600

    Add fig.sh.

---
+++ docker/fig.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# note: must be run from the docker directory
+mkdir -p m2
+mkdir -p lein
+docker run -ti --rm -p 127.0.0.1:7896:7897 -p 127.0.0.1:3449:3449 -v $(pwd -P)/..:/root/docker -v $(pwd -P)/m2:/root/.m2 -v $(pwd -P)/lein:/root/.lein -v $(pwd -P)/../../utils:/root/docker/checkouts/utils tincan /bin/bash
--- docker/run.sh
+++ docker/run.sh
@@ -2,4 +2,4 @@
 # note: must be run from the docker directory
 mkdir -p m2
 mkdir -p lein
-docker run -ti --rm -p 127.0.0.1:7897:7897 -p 127.0.0.1:3449:3449 -v $(pwd -P)/..:/root/docker -v $(pwd -P)/m2:/root/.m2 -v $(pwd -P)/lein:/root/.lein clojure:latest /bin/bash
+docker run -ti --rm -p 127.0.0.1:7897:7897 -p 127.0.0.1:3500:3500 -v $(pwd -P)/..:/root/docker -v $(pwd -P)/m2:/root/.m2 -v $(pwd -P)/lein:/root/.lein -v $(pwd -P)/../../utils:/root/docker/checkouts/utils tincan /bin/bash
